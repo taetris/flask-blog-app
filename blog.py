962c53f4f260513cb8824d93b46836b0f2d19292
taetris <triptis1235@gmail.com>
2022-11-28 23:00:36 +0545

    templates and static folders added

--- blog.py
+++ blog.py
@@ -1,8 +1,9 @@
 # Making venv: pip install -> virtualenv <name> -> source <name>/bin/activate -> deactivate
 from flask import Flask
 
+#  URL: http://127.0.0.1:5000/
 app = Flask(__name__)
 
-@app.route("/")
-def begin():
-    return "fwgrgergeg"
+@app.route('/')
+def blog():
+    return "Welcome to this Flask Generated Page"
